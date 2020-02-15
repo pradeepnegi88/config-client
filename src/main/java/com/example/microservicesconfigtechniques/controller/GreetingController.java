fdce1ddd819b99b36843246c61511788f8e18665
PradeepNegi <pradeepnegi2012@gmail.com>
2020-02-15 17:27:18 +0530

    Added properties files different techniques to set values

--- src/main/java/com/example/microservicesconfigtechniques/controller/GreetingController.java
+++ src/main/java/com/example/microservicesconfigtechniques/controller/GreetingController.java
@@ -4,13 +4,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class GreetingController {
-    @Value("${app.message}")
+    @Value("${app.message: default value}")
     private String greetingMessage;
+
+    @Value(" This is static message ")
+    private String staticMessage;
+    @Value("${app.list}")
+    private List<String> stringList;
 
     @GetMapping(path = "/greeting")
     public String getGretting() {
-        return greetingMessage;
+        return greetingMessage+ staticMessage+ stringList;
     }
 }
