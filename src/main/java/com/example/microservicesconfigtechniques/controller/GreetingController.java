ba53e2461ef49cd8e7e0c3f207bdc795fe77621f
PradeepNegi <pradeepnegi2012@gmail.com>
2020-02-16 11:42:49 +0530

    Added refresh scope to update the config properties without restart using post actuator/refresh

--- src/main/java/com/example/microservicesconfigtechniques/controller/GreetingController.java
+++ src/main/java/com/example/microservicesconfigtechniques/controller/GreetingController.java
@@ -3,13 +3,14 @@ package com.example.microservicesconfigtechniques.controller;
 import com.example.microservicesconfigtechniques.config.DatabaseConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
-
+@RefreshScope
 @RestController
 public class GreetingController {
     @Value("${app.message: default value}")
